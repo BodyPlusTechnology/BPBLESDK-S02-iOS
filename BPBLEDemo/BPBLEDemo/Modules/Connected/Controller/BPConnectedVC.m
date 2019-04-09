3d8e86ec1fa4b2d971ad335f00ddf9574a3eb9c2
GrayLand <guilin.lan@bodyplus.cc>
2019-04-09 17:21:01 +0800

    Fixed

--- BPBLEDemo/BPBLEDemo/Modules/Connected/Controller/BPConnectedVC.m
+++ BPBLEDemo/BPBLEDemo/Modules/Connected/Controller/BPConnectedVC.m
@@ -469,7 +469,7 @@ typedef NS_ENUM(NSUInteger, CellType) {
 
 #pragma mark - Getter
 
-- (UITableView *)tableView {g
+- (UITableView *)tableView {
     if (!_tableView) {
         _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
         _tableView.delegate = self;
